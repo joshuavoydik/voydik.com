b49ef39d9015f5475b8ae6d460afeff11824e154
Joshua <voydik@gmail.com>
2023-07-25 11:52:04 -0700

    footer

--- theme.config.js
+++ theme.config.js
@@ -3,15 +3,15 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Joshua Voydik
+      <time>{YEAR}</time> © Joshua Voydik 
       <style jsx>{`
         a {
           float: right;
         }
         @media screen and (max-width: 480px) {
           article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
+            padding-top: 0.5rem;
+            padding-bottom: 1rem;
           }
         }
       `}</style>
